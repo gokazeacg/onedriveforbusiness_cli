97da9f019d21fe16fe3efdf8422ea9c3c093416a
gokazeacg <56425629+gokazeacg@users.noreply.github.com>
2021-12-07 17:05:29 +0800

    Update od_get_access_token.py

--- od_get_access_token.py
+++ od_get_access_token.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from urllib import parse
+import time
 
 print('輸入目錄 (租用戶) 識別碼：',end='')
 tenant_ID = str(input())
@@ -14,13 +15,17 @@ print('打開下列網址：'+url)
 print('輸入登入後顯示的網址：',end='')
 redirect_url = input()
 code = redirect_url.split('&')[0].split('=')[1]
-
+sol = time.time()
 HEADERS = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
 url = 'https://login.microsoftonline.com/'+tenant_ID+'/oauth2/v2.0/token'
 FormData = {'client_id':client_id,'scope':'https://graph.microsoft.com/Files.ReadWrite offline_access','redirect_uri':'http://localhost:4452/','client_secret':client_secret,'grant_type':'authorization_code','code':code}
 data = parse.urlencode(FormData)
 retur = json.loads(requests.post(url=url, headers=HEADERS, data=data).text)
 token = retur['access_token']
-print('你的訪問令牌為：'+token)
+#print('你的訪問令牌為：'+token)
 refresh_token = retur['refresh_token']
-print('你的刷新令牌為：'+refresh_token)
+#print('你的刷新令牌為：'+refresh_token)
+x = str(sol)+','+tenant_ID+','+client_id+','+client_secret+','+token+','+refresh_token
+f = open('.token','r+')
+f.write(x)
+f.close()
