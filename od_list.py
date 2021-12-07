84efc89f1292de14414e451ff8b906ee5d7e40b7
gokazeacg <56425629+gokazeacg@users.noreply.github.com>
2021-12-07 17:27:15 +0800

    Update od_list.py

--- od_list.py
+++ od_list.py
@@ -2,34 +2,37 @@ import requests
 import json
 from urllib import parse
 import os
+import time
 
-print('輸入目錄 (租用戶) 識別碼：',end='')
-tenant_ID = str(input())
-print('輸入應用程式 (用戶端) 識別碼：',end='')
-client_id = str(input())
-print('輸入用戶端密碼：',end='')
-client_secret = str(input())
+f = open('.token','r')
+fread = f.read()
+f.close()
+sol = fread.split(',')[0]
+tenant_ID = fread.split(',')[1]
+client_id = fread.split(',')[2]
+client_secret = fread.split(',')[3]
+access_token = fread.split(',')[4]
+refresh_token = fread.split(',')[5]
 
-def get_token():
-    HEADERS = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+def refresh():
+    sol = time.time()
     url = 'https://login.microsoftonline.com/'+tenant_ID+'/oauth2/v2.0/token'
-    FormData = {'client_id':client_id,'scope':'.default','client_secret':client_secret,'grant_type':'client_credentials'}
+    HEADERS = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+    FormData = {'client_id':client_id,'scope':'https://graph.microsoft.com/Files.ReadWrite offline_access','redirect_uri':'http://localhost:4452/','client_secret':client_secret,'grant_type':'refresh_token','refresh_token':refresh_token}
     data = parse.urlencode(FormData)
-    content = requests.post(url=url, headers=HEADERS, data=data).text
-    content = json.loads(content)['access_token']
-    print('你的金鑰為：'+content)
-    return content
+    retur = json.loads(requests.post(url=url, headers=HEADERS, data=data).text)
+    access_token = retur['access_token']
+    #print('你的訪問令牌為：'+access_token)
+    refresh_token = retur['refresh_token']
+    #print('你的刷新令牌為：'+refresh_token)
+    x = str(sol)+','+tenant_ID+','+client_id+','+client_secret+','+access_token+','+refresh_token
+    f = open('.token','r+')
+    f.write(x)
+    f.close()
+    return access_token
 
-if '.token' in os.listdir():
-    file = open('./.token','r')
-    content = file.read()
-    file.close()
-else:
-    file = open('./.token','w')
-    content = get_token()
-    file.write(content)
-    file.close()
-
+if float(sol) + 3600 < time.time():
+    access_token = refresh()
 headers = {'Authorization': 'Bearer '+content}
 url = 'https://graph.microsoft.com/v1.0/users'
 usersjson = json.loads(requests.get(url, headers=headers).text)
